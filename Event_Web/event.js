11fa0c72c576810fc522f6559706a0224f6c91c4
Vineeth Sagar H L <156885142+Vineeth-Sagar@users.noreply.github.com>
2024-11-14 19:00:40 +0530

    Delete Event_Web/event.js

--- Event_Web/event.js
+++
@@ -1,6 +0,0 @@
-// Add click event to the "Let's Go" button to display the dashboard
-document.getElementById("letsGoBtn").addEventListener("click", function() {
-    document.querySelector(".background").style.display = "none";  // Hide background section
-    document.getElementById("dashboard").classList.remove("hidden");  // Show dashboard
-    document.getElementById("dashboard").classList.add("show");
-});
